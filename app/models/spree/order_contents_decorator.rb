9f58222dc917b6eabb6b5158b9b60287917deee8
Andreas Salme <asalme@yesvideo.com>
2014-10-27 09:57:05 -0700

    make gift cards work with flexi variants

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,12 +1,14 @@
 Spree::OrderContents.class_eval do
 
   durably_decorate :grab_line_item_by_variant, mode: 'soft', sha: '5606323a3e50e650b2ad1a51c43ba54ebc469b72' do |variant, *args|
-    raise_error = args[0] || false
+    raise_error = args[0]             || false
+    ad_hoc_option_value_ids = args[1] || []
+    product_customizations = args[2]  || []
 
     if variant.product.is_gift_card?
       line_item = nil
     else
-      line_item = order.find_line_item_by_variant(variant)
+      line_item = order.find_line_item_by_variant(variant, ad_hoc_option_value_ids, product_customizations)
     end
 
     if !line_item.present? && raise_error
